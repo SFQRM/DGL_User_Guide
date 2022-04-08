7e7441bf6ae06fc35d32603392b4fc98aaee8795
SFQRM <zhuojinghan@126.com>
2022-04-08 23:07:44 +0800

    20220408 update

--- Message_Passing.py
+++ Message_Passing.py
@@ -16,6 +16,40 @@ print(cuda_g.ndata['hv'])
 # print(cuda_g.ndata['he'])
 """
 
+# 2.1 内置函数和消息传递API
+
+"""
+    要对源节点的hu特征和目的节点的hv特征求和，然后将结果保存在边的he特征上。
+"""
+import dgl.function as fn
+
+N = 6  # 节点数目
+u, v = torch.tensor([1, 1, 1, 2, 2, 4, 0, 5, 5]), torch.tensor([2, 3, 4, 3, 5, 3, 1, 3, 4])
+g = dgl.graph((u, v))                      # 生成DGL图
+g.ndata["ht"] = torch.ones(N) / N  # 将每个节点的hu值初始化为1/N
+# g.ndata["hv"] = g.out_degrees(g.nodes()).float()  # 将每个节点的out-degree存储为节点特征hv
+weights = torch.tensor([1., 1., 1., 1., 1., 1., 1., 1., 1.])   # 每条边的权重
+g.edata["a"] = weights
+# 内置的消息传递函数
+# fn.u_add_v("hu", "hv", "m")
+
+# 内置的消息聚合函数
+# fn.sum("m", "he")
+
+
+def update_all_example(g):
+    g.update_all(fn.u_mul_e("ht", "a", "m"),
+             fn.sum("m", "ht"))
+    final_ht = g.ndata["ht"] * 2
+    return final_ht
+
+
+for i in range(5):
+    print(update_all_example(g))
+    
+# print(final_ht)
+"""
+
 # 2.2 编写高效的消息传递代码
 
 u, v = torch.tensor([0, 0, 0, 1]), torch.tensor([1, 2, 3, 3])
@@ -33,3 +67,4 @@ def concat_message_function(edges):
 
 g.apply_edges(concat_message_function)
 g.edata['out'] = g.edata['cat_feat'] @ linear
+"""
--- PageRank.py
+++ PageRank.py
@@ -2,26 +2,71 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import torch
 import dgl
+import dgl.function as fn
 
-N = 10          # 节点数目
-DAMP = 0.85     # 阻尼因子
-K = 10          # 迭代次数
-g = nx.nx.erdos_renyi_graph(N, 0.2)         # 图随机生成器，生成nx图
-g = dgl.from_networkx(g)                    # 转换成DGL图
-# print(g)
+N = 6  # 节点数目
+DAMP = 0.85  # 阻尼因子
+K = 10  # 迭代次数
+# g = nx.nx.erdos_renyi_graph(N, 0.2)  # 图随机生成器，生成nx图
+# g = dgl.from_networkx(g)  # 转换成DGL图
+u, v = torch.tensor([1, 1, 1, 2, 2, 4, 0, 5, 5]), torch.tensor([2, 3, 4, 3, 5, 3, 1, 3, 4])
+g = dgl.graph((u, v))                      # 生成DGL图
+print(g)
 # g = g.to('cuda:0')
 
-'''
-plt.figure()            # 创建一个图形对象
-plt.subplot(111)        # 创建一个子图,行列
+plt.figure()  # 创建一个图形对象
+plt.subplot(111)  # 创建一个子图,行列
 nx.draw(g.to_networkx(),
-        node_size=250,                      # 设置节点大小
-        node_color=[[.75, .75, .75]])       # 设置节点灰度值
+        node_size=250,  # 设置节点大小
+        node_color=[[.75, .75, .75]])  # 设置节点灰度值
 plt.show()
-'''
 
-g.ndata['pv'] = torch.ones(N)/N                     # 将每个节点的PageRank值初始化为1/N
-g.ndata['deg'] = g.out_degrees(g.nodes()).float()   # 将每个节点的out-degree存储为节点特征
+g.ndata["pagerank"] = torch.ones(N) / N  # 将每个节点的PageRank值初始化为1/N
+g.ndata["degree"] = g.out_degrees(g.nodes()).float()  # 将每个节点的out-degree存储为节点特征
 
-print(g.ndata['pv'])
-print(g.ndata['deg'])
+print(g.ndata['pagerank'])
+print(g.ndata['degree'])
+
+
+"""
+# 定义message函数，它将每个节点的PageRank值除以其out-degree，并将结果作为消息传递给它的邻居：
+def pagerank_message_func(edges):
+    return {"pv": edges.src["pagerank"] / edges.src["degree"]}
+
+
+# 定义reduce(聚合)函数，它从mailbox中删除并聚合message，并计算其新的PageRank值：
+def pagerank_reduce_func(nodes):
+    msgs = torch.sum(nodes.mailbox["pv"], dim=1)
+    pv = (1 - DAMP) / N + DAMP * msgs
+    return {"pv": pv}
+"""
+
+# g.register_message_func(pagerank_message_func)
+# g.register_reduce_func(pagerank_reduce_func)
+
+"""
+def pagerank_naive(g):
+    # Phase #1: 沿所有边缘发送消息。
+    for u, v in zip(*g.edges()):
+        g.send((u, v))
+    # Phase #2: 接收消息以计算新的PageRank值。
+    for v in g.nodes():
+        g.recv(v)
+
+
+pagerank_naive(g)
+print(g.ndata["pagerank"])
+"""
+
+
+def paggrank_build(g):
+    g.ndata["pagerank"] = g.ndata["pagerank"] / g.ndata["degree"]
+    g.update_all(message_func=fn.copy_src(src="pagerank", out="m"),
+                 reduce_func=fn.sum(msg="m", out = "m_sum"))
+    g.ndata["pagerank"] = (1 - DAMP) / N + DAMP * g.ndata["m_sum"]
+
+
+for i in range(5):
+    paggrank_build(g)
+    print(g.ndata["pagerank"])
+
