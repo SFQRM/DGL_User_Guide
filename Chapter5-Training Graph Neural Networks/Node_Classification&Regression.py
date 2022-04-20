3302ce25fc0f9c1290984553867706a8f2ef03f7
SFQRM <zhuojinghan@126.com>
2022-04-20 21:15:05 +0800

    20220420 update

--- Chapter5-Training Graph Neural Networks/Node_Classification&Regression.py
+++ Chapter5-Training Graph Neural Networks/Node_Classification&Regression.py
@@ -4,13 +4,20 @@ import dgl.nn as dglnn
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 """
     加载图数据
 """
 # ----------- 0. 加载数据集 ----------- #
 dataset = dgl.data.CiteseerGraphDataset(raw_dir="./data")
-graph = dataset[0]
+# dataset = dgl.data.CoraGraphDataset(raw_dir="./data")
+# dataset = dgl.data.PubmedGraphDataset(raw_dir="./data")
+graph = dataset[0].to(device)
 # print("graph:", graph)
 
 
@@ -23,6 +30,7 @@ node_labels = graph.ndata['label']
 train_mask = graph.ndata['train_mask']
 valid_mask = graph.ndata['val_mask']
 test_mask = graph.ndata['test_mask']
+
 n_features = node_features.shape[1]                     # 特征的维度：3703
 n_labels = int(node_labels.max().item() + 1)            # 标签的数量：6
 
@@ -58,6 +66,7 @@ class SAGE(nn.Module):
 
 
 model = SAGE(in_feats=n_features, hid_feats=100, out_feats=n_labels)
+model = model.to(device)
 # print(model)
 
 
@@ -67,6 +76,7 @@ model = SAGE(in_feats=n_features, hid_feats=100, out_feats=n_labels)
 """
 # ----------- 3. 设置优化器 ----------- #
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+# optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 # print(optimizer)
 
 
@@ -92,9 +102,15 @@ for epoch in range(100):
 
     # 计算损失值
     loss = F.cross_entropy(logits[train_mask], node_labels[train_mask])
+    # 将训练过程中的loss保存到文件
+    with open('loss_records.txt', 'a') as f:
+        f.write('%d %.6f\n' % (epoch, loss))
 
     # 计算验证集的准确度
     acc = evaluate(model, graph, node_features, node_labels, valid_mask)
+    # 将训练过程中的验证集acc保存到文件
+    with open('acc_records.txt', 'a') as f:
+        f.write('%d %.6f\n' % (epoch, acc))
 
     # 进行反向传播计算
     optimizer.zero_grad()
@@ -104,6 +120,31 @@ for epoch in range(100):
     print('In epoch {}, loss: {}'.format(epoch, loss.item()))
 
 
+# ----------- 6. 画loss和acc曲线 ----------- #
+def drawing_curves():
+    acc_data = np.loadtxt("acc_records.txt")
+    loss_data = np.loadtxt("loss_records.txt")
+
+    acc_x = acc_data[:, 0]
+    acc_y = acc_data[:, 1]
+    loss_x = loss_data[:, 0]
+    loss_y = loss_data[:, 1]
+
+    fig = plt.figure(figsize=(7, 5))
+    ax1 = fig.add_subplot(1, 1, 1)  # ax1是子图的名字
+
+    # "g"代表green，表示画出的曲线是绿色，"-"表示画出的曲线是实线，label表示图例的名称
+    plt.plot(loss_x, loss_y, 'r-', label=u'loss')
+    plt.plot(acc_x, acc_y, 'b-', label=u'acc')
+    plt.legend()
+
+    plt.xlabel(u'epoch')
+    plt.ylabel(u'loss')
+    plt.title('Compare loss for different models in training.')
+    plt.show()
+
+
+drawing_curves()
 print('Accuracy', acc)
 
 # 如果需要的话，保存训练好的模型。本例中省略。
